65bc8393a796f8ceafe6623a3bd7b4fe6ab45f51
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-06-24 23:05:32 +1000

    updating main

---
+++ Assignment01/main.cpp
@@ -0,0 +1,5 @@
+/*  
+ * In this assignment, the task is to compare the avg length between 2 algorithms
+ * and shannon_entropy. So consider the Huffman algorithm, there is no need to create
+ * the whole huffman tree.
+ */
